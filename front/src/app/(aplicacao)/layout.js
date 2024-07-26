80b87dd09473719d8a4d3f9bd641e345fcdb0c21
luizmarcelolm <luizmarcellolm@gmail.com>
2024-07-26 18:23:24 -0300

    criação menu home

--- front/src/app/(aplicacao)/layout.js
+++ front/src/app/(aplicacao)/layout.js
@@ -1,5 +1,6 @@
-import './globals.css';
+import '../globals.css';
 import {Inter} from 'next/font/google';
+import Menu from '../../components/Menu';
 
 const inter = Inter ({ subsets: ['latin']});
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>
+      <Menu/>
         {children}</body>
     </html>
   )
--- front/src/components/Menu/index.jsx
+++ front/src/components/Menu/index.jsx
@@ -1,6 +1,31 @@
+'use client';
+
+import './style.css'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
 const Menu = () => {
+    const [ menulateral, setMenuLateral ] = useState(false)
+    const router = useRouter()
+
     return (
-        <nav>Menu</nav>
+        <>
+        <nav className='menu'>
+             <button onClick={() => setMenuLateral(!menulateral)}>Menu</button>
+             <div>Logo</div>
+             <button onClick={() => router.push('/login')}>Login</button>
+        </nav>
+
+        { menulateral &&
+           <div className='menu_lateral'>
+            <div className='fechar' onClick={() => setMenuLateral(!menulateral)}>X</div>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+          </div>}
+       </>
     )
 }
 export default Menu;
