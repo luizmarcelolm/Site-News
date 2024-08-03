0355f78bc1d744ba090ea68cb0fb9a07f56688cc
luizmarcelolm <luizmarcellolm@gmail.com>
2024-08-03 10:50:47 -0300

    criação página criação noticias

---
+++ front/src/components/CadastroNoticiaForm/index.jsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useState } from "react";
+
+const CadastroNoticiaForm = () => {
+    const [titulo, setTitulo] = useState('')
+    const [img, setImg] = useState('')
+    const [texto, setTexto] = useState('')
+
+    const aoSubmeter = (e) => {
+        e.preventDefault();
+        console.log("submeteu", titulo,img, texto)
+    }
+
+    const aoAlterarValores = (e) => {
+        const {name, value} = e.target;
+        if(name === 'titulo') {
+            setTitulo(value)
+        }
+
+        if(name === 'img'){
+            setImg(value)
+        }
+
+        if(name === 'texto') {
+            setTexto(value)
+        }
+    }
+
+    return(
+        <form onSubmit={aoSubmeter}>
+            <div>
+                <label htmlFor="titulo">Título</label>
+                <input type="text" name="titulo" onChange={aoAlterarValores}/>
+            </div>
+            <div>
+                <label htmlFor="img">Imagem</label>
+                <input type="text" name="img" onChange={aoAlterarValores}/>
+            </div>
+            <div>
+                <label htmlFor="texto">Texto</label>
+                <textarea type="text" name="texto" onChange={aoAlterarValores}/>
+            </div>
+            <button type='submit'>Criar notícia</button>
+        </form>
+          
+    )
+}
+
+export default CadastroNoticiaForm
