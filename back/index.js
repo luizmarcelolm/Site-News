3216542f34505efd347690d36de019f4ab5c9ba4
luizmarcelolm <luizmarcellolm@gmail.com>
2024-08-06 13:18:19 -0300

    criação de rotas

--- back/index.js
+++ back/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+app.get('/usuarios', (req, res) => {
+    return res.status(200).json([]);
+});
+
+app.post('/usuarios', (req, res) =>{
+    return res.status(201).json([]);
+});
+
+app.get('/noticias', (req, res) =>{
+    return res.status(200).json([]);
+});
+
+app.post('/noticias', (req, res) =>{
+    return res.status(201).json([]);
+});
+
 app.listen(8080, () => {
     console.log('Servidor funcionando na porta 8080');
 });
