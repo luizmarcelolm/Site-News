34c3da6d9e864d4830d41190a24d16ee9b12cd76
luizmarcelolm <luizmarcellolm@gmail.com>
2024-08-12 22:19:59 -0300

    criação de sincronização

--- front/src/app/(aplicacao)/home/page.jsx
+++ front/src/app/(aplicacao)/home/page.jsx
@@ -1,28 +1,27 @@
-import Noticia from '../../../components/Noticia'
+'use client'
 
-const noticias = [
-    {
-        id:1,
-        titulo: 'Noticia 1',
-        img: 'https://via.placeholder.com/150',
-        texto: 'Texto da <b>noticia 1</b>'
-    },
-    {
-        id:2,
-        titulo: 'Noticia 2',
-        img: 'https://via.placeholder.com/150',
-        texto: 'Texto da <b>noticia 2</b>'
-    },
-    {
-        id:3,
-        titulo: 'Noticia 3',
-        img: 'https://via.placeholder.com/150',
-        texto: 'Texto da <b>noticia 3</b>'
-    }
-]
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import Noticia from '../../../components/Noticia';
 
 
 const HomePage = () => {
+    const [noticias, setNoticias] = useState([]);
+
+    const getNoticias = async () => {
+
+        try {
+            const result = await axios.get('http://localhost:8080/noticias')
+            setNoticias(result.data);
+        } catch (error) {
+            alert(error.response.data.message);
+        }
+       
+    }
+    useEffect(() => {
+        getNoticias()
+    }, []);
+
     return (
         <div>
             <h1>Home</h1>
--- front/src/components/CadastroNoticiaForm/index.jsx
+++ front/src/components/CadastroNoticiaForm/index.jsx
@@ -1,15 +1,29 @@
 'use client'
-
-import { useState } from "react";
+import axios from 'axios'
+import { useState } from "react"
 
 const CadastroNoticiaForm = () => {
     const [titulo, setTitulo] = useState('')
     const [img, setImg] = useState('')
     const [texto, setTexto] = useState('')
+    const [categoria, setCategoria] = useState('')
 
-    const aoSubmeter = (e) => {
+    const aoSubmeter = async (e) => {
         e.preventDefault();
-        console.log("submeteu", titulo,img, texto)
+        try{
+            const formulario = {
+                titulo,
+                img,
+                texto,
+                categoria
+            }
+            const result = await axios.post('http://localhost:8080/noticias', formulario)
+            alert("Nova noticia cadastrada")
+            return router.push('/home')
+   
+          } catch (error){
+            alert(error.response.data.message)
+          }
     }
 
     const aoAlterarValores = (e) => {
@@ -24,6 +38,10 @@ const CadastroNoticiaForm = () => {
 
         if(name === 'texto') {
             setTexto(value)
+        }
+
+        if(name === 'categoria') {
+            setCategoria(value)
         }
     }
 
@@ -40,6 +58,15 @@ const CadastroNoticiaForm = () => {
             <div>
                 <label htmlFor="texto">Texto</label>
                 <textarea type="text" name="texto" onChange={aoAlterarValores}/>
+            </div>
+            <div>
+                <label htmlFor="categoria">Categoria</label>
+                <select name="categoria" onChange={aoAlterarValores}>
+                    <option value="produto">Produto</option>
+                    <option value="tecnologia">Tecnologia</option>
+                    <option value="rh">RH</option>
+                    <option value="vendas">Vendas</option>
+                </select>
             </div>
             <button type='submit'>Criar notícia</button>
         </form>
